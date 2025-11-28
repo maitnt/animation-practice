185e0bd6986edeead9f38f1976d28e0b81af1a89
trang.nguyen <maitrangnt2210@gmail.com>
2025-11-28 21:05:59 +0700

    chore: update text

--- assert/script.js
+++ assert/script.js
@@ -117,7 +117,7 @@ function confetiBalloons() {
     hw = w / 2;
   (hh = h / 2),
     (opts = {
-      strings: ["HAPPY", "BIRTHDAY", "❤ Hieu dep trai ❤"],
+      strings: ["HAPPY", "BIRTHDAY", "❤ Anh dep trai ❤"],
       charSize: 30,
       charSpacing: 35,
       lineHeight: 40,
